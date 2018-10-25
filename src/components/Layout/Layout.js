cb9c687fedd9d5089b25ad18aa587dbf7cc0c41e
Nishant Rao <nishanthrao29@gmail.com>
2018-10-25 12:45:35 +0530

    minor chnages

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,15 +3,26 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import './Layout.css'
 
-const layout = (props) => (
-  <div>
-    <Toolbar/>
-    <SideDrawer/>
-    <main className="sub-container">
-      {props.children}
-    </main>
-  </div>
-);
+export default class Layout extends React.Component{
+  state = {
+    showSideDraw : true
+  }
 
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDraw : false
+    });
+  }
+  render(){
+    return(
+      <div>
+        <Toolbar/>
+        <SideDrawer open={this.state.showSideDraw} closed={this.sideDrawerClosedHandler}/>
+        <main className="sub-container">
+        {this.props.children}
+        </main>
+      </div>
 
-export default layout;
+    )
+  }
+}
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import './SideDrawer.css';
 
 const sideDrawer = (props) => {
-
+  let attachedClasses = [SideDrawer, Close];
+  if(props.open){
+    attachedClasses = [SideDrawer, Open];
+  }
   return(
-    <div className="SideDrawer">
-        <div style={{height:"11%",marginBottom:"32px"}}>
-          <Logo/>
-        </div>
-      <nav>
-        <NavigationItems/>
-      </nav>
+    <div>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+          <div style={{height:"11%",marginBottom:"32px"}}>
+            <Logo/>
+          </div>
+        <nav>
+          <NavigationItems/>
+        </nav>
+      </div>
     </div>
   );
 };
