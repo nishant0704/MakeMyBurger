f0d912543290775901d7c15fd68ece492744ff8b
Nishant0704 <nishanthrao29@gmail.com>
2019-08-09 18:12:39 +0530

    routing forward and goBack

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -81,7 +81,8 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        this.props.history.push('/checkout');
     }
 
     render () {
